e5e792df5d6c6b999acaa39a8e78fa3d14ab7966
alcat2008 <ge_yangyang@126.com>
2016-05-19 16:36:05 +0800

    update eslint rules package provided by airbnb

--- bigpipe/app.js
+++ bigpipe/app.js
@@ -1,41 +1,41 @@
-var express = require('express'),
-  fs      = require('fs'),
-  async   = require('async');
+var express = require('express');
+var fs = require('fs');
+var async = require('async');
 
 var server = express();
 
 server.use('/bigpipe', express.static(__dirname + '/static'));
 server.use('/lib', express.static(__dirname + '/bower_components'));
 
-server.listen(4000, function(){
+server.listen(4000, function () {
   console.log('server is listen on port 4000......');
-})
+});
 
-server.get('/info', function(req, res, next){
-
+server.get('/info', function (req, res, next) {
   var content = fs.readFileSync(__dirname + '/index.html', 'utf8');
 
-  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
   res.write(content);
 
   // 下面开始加载文件内容
   // 开始并行加载多个文件
-  async.parallel([function(callback){
-    fs.readFile(__dirname + '/data/users.txt', 'utf8', function(err, data){
-      data = data.replace(/\n/g, '\\n');
-      res.write('<script>bigpipe.set("users","'+ data +'");</script>');
-      callback(err, data);
+  async.parallel([function (callback) {
+    fs.readFile(__dirname + '/data/users.txt', 'utf8', function (err, data) {
+      var tmpData = data.replace(/\n/g, '\\n');
+      res.write('<script>bigpipe.set("users","' + tmpData + '");</script>');
+      callback(err, tmpData);
     });
-  },function(callback){
-    fs.readFile(__dirname + '/data/info.txt', 'utf8', function(err, data){
-      data = data.replace(/\n/g, '\\n');
-      console.log(data);
-      res.write('<script>bigpipe.set("info","' + data + '");</script>');
-      callback(err, data);
-    })
-  }],function(err, result){
+  }, function (callback) {
+    fs.readFile(__dirname + '/data/info.txt', 'utf8', function (err, data) {
+      var tmpData = data.replace(/\n/g, '\\n');
+      console.log(tmpData);
+      res.write('<script>bigpipe.set("info","' + tmpData + '");</script>');
+      callback(err, tmpData);
+    });
+  }], function (err, result) {
     // if(err) res.end();
     res.end();
   });
-
 });
