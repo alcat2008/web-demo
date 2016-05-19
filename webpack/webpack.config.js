e6dbac75351041590388871f455cee19fc489b31
alcat2008 <ge_yangyang@126.com>
2016-05-19 18:42:50 +0800

    add webpack demo

---
+++ webpack/webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+
+module.exports = {
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:8080',
+    path.resolve(__dirname, 'app/main.js')
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    // sourceMapFilename: '[file].map',
+    // devtoolLineToLine: true
+  },
+  // devtool: '#cheap-module-eval-source-map'
+};
