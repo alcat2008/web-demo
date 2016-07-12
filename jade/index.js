feb1f52d708904e06847a136182d92c2f1e5e5a4
alcat2008 <ge_yangyang@126.com>
2016-07-12 10:19:22 +0800

    add demo for jade

---
+++ jade/index.js
@@ -0,0 +1,38 @@
+/**
+ * Module dependencies
+ */
+
+var express = require('express');
+var stylus = require('stylus');
+var nib = require('nib');
+
+var app = express();
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
+}
+
+app.set('view', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.use(express.logger('dev'));
+app.use(stylus.middleware(
+  {
+    src: __dirname + '/public',
+    compile: compile
+  }
+));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  // res.send('Hi there!');
+
+  res.render('index',
+    { title : 'Home' }
+  );
+});
+
+app.listen(3000);
