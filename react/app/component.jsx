3f83115024b6e5cec07ef314acced0a9c8d01e4e
alcat2008 <ge_yangyang@126.com>
2016-07-29 16:02:17 +0800

    demo to setState for object properties

--- react/app/component.jsx
+++ react/app/component.jsx
@@ -2,11 +2,32 @@ import React from 'react';
 import './component.css';
 
 export default class Hello extends React.Component {
+
+  constructor(props) {
+    super(props);
+    // initial state
+    this.state = {
+      info: {
+        message: 'this is demo page, will be update after 3s'
+      }
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        info: {
+          message: 'message updated'
+        }
+      });
+    }, 10000);
+  }
+
   render() {
     return (
       <div className="test-class">
         <h1>Hello React</h1>
-        <div>this is demo page</div>
+        <div>{this.state.info.message}</div>
       </div>
     );
   }
