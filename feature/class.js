23922f59166c01eea3102bd3d03c4a85a3735ffe
alcat2008 <ge_yangyang@126.com>
2016-08-18 09:49:46 +0800

    add class feature test

---
+++ feature/class.js
@@ -0,0 +1,23 @@
+
+class PointClass {
+  constructor(x, y) {
+    // ...
+  }
+
+  toString() {
+    // ...
+  }
+}
+
+var PointPrototype = function (x, y) {
+  // ...
+}
+
+PointPrototype.prototype.toString = function () {
+  // ...
+}
+
+export {
+  PointClass,
+  PointPrototype
+};
---
+++ feature/feature.test.js
@@ -0,0 +1,25 @@
+
+import  * as es6 from './module';
+import es6Default from './module';
+import { a, b } from './module';
+// const { a, b } = es6;
+
+import { PointClass, PointPrototype } from './class';
+
+describe('feature', () => {
+  // it('es6 module', () => {
+  //   console.log(es6);
+  //   console.log(es6Default);
+  //   console.log(a);
+  //   console.log(b);
+  // });
+
+  it('es6 class', () => {
+    console.log(Object.keys(PointClass.prototype));
+    console.log(Object.getOwnPropertyNames(PointClass.prototype));
+
+    console.log(Object.keys(PointPrototype.prototype));
+    console.log(Object.getOwnPropertyNames(PointPrototype.prototype));
+  });
+});
+
