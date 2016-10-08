2fbedad6e08730dec9dc958e8f4826fd1b0d42d5
alcat2008 <ge_yangyang@126.com>
2016-10-08 10:16:06 +0800

    test typescript class convertion

--- typescript/test.ts
+++ typescript/test.ts
@@ -1,3 +1,19 @@
 
 export type TabsType = 'line' | 'card' | 'editable-card'
 export type TabsPosition = 'top' | 'right' | 'bottom' | 'left';
+
+enum Color {Red = 1, Green, Blue};
+let c: Color = Color.Green;
+
+class Greeter {
+  greeting: string;
+  constructor(message: string) {
+    this.greeting = message;
+  }
+  greet() {
+    return "Hello, " + this.greeting;
+  }
+}
+
+let greeter = new Greeter("world");
+
