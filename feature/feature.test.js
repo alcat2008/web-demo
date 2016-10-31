1d850dfa58cc727a42e05d6dd53f45558c3fb980
alcat2008 <ge_yangyang@126.com>
2016-10-31 18:06:46 +0800

    test object and array spread effection

--- feature/feature.test.js
+++ feature/feature.test.js
@@ -30,5 +30,27 @@ describe('feature', () => {
   it('es6 decorator', () => {
     console.log(TestableClass.isTestable);
   });
+
+  it('test object and array', () => {
+    const { test1, demo1, struct1 } = { test1: 'test1', demo1: 'demo1', struct1: 'struct1' };
+    let beforeTime1 = new Date();
+    console.log(test1);
+    console.log(demo1);
+    console.log(struct1);
+    let endTime1 = new Date();
+    console.log('object test: ', beforeTime1.getTime());
+    console.log('object test: ', endTime1.getTime());
+
+
+    const [test2, demo2, struct2] = ['test2', 'demo2', 'struct2'];
+    let beforeTime2 = new Date();
+    console.log(test2);
+    console.log(demo2);
+    console.log(struct2);
+    let endTime2 = new Date();
+    console.log('array test: ', beforeTime2.getTime());
+    console.log('array test: ', endTime2.getTime());
+
+  });
 });
 
