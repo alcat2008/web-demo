cf88e79c65340ae9aec0f3400674addf5e205c5e
alcat2008 <ge_yangyang@126.com>
2017-03-10 16:01:11 +0800

    feat: add drag example using ant-motion

---
+++ drag/src/Drag.js
@@ -0,0 +1,69 @@
+/* eslint-disable no-console */
+
+import React from 'react';
+import ListSort from './ListSort';
+
+import './drag.css';
+
+const dataArray = [
+  {
+    icon: 'question-circle-o',
+    color: '#FF5500',
+    title: 'Senior Product Designer',
+    text: 'Senior Product Designer',
+  },
+  {
+    icon: 'plus-circle-o',
+    color: '#5FC296',
+    title: 'Senior Animator',
+    text: 'Senior Animator',
+  },
+  {
+    icon: 'check-circle-o',
+    color: '#2DB7F5',
+    title: 'Visual Designer',
+    text: 'Visual Designer',
+  },
+  {
+    icon: 'cross-circle-o',
+    color: '#FFAA00',
+    title: 'Computer Engineer',
+    text: 'Computer Engineer',
+  },
+];
+
+export default class ListSortDemo extends React.Component {
+  static propTypes = {
+    className: React.PropTypes.string,
+  };
+
+  static defaultProps = {
+    className: 'list-sort-demo',
+  };
+
+  render() {
+    const childrenToRender = dataArray.map((item, i) => {
+      const { title, text } = item;
+      return (
+        <div key={i} className={`${this.props.className}-list`}>
+          <div className={`${this.props.className}-icon`}>
+          </div>
+          <div className={`${this.props.className}-text`}>
+            <h1>{title}</h1>
+            <p>{text}</p>
+          </div>
+        </div>
+      );
+    });
+    return (<div className={`${this.props.className}-wrapper`}>
+      <div className={this.props.className}>
+        <ListSort
+          dragClassName="list-drag-selected"
+          appearAnim={{ animConfig: { marginTop: [5, 30], opacity: [1, 0] } }}
+        >
+          {childrenToRender}
+        </ListSort>
+      </div>
+    </div>);
+  }
+}
