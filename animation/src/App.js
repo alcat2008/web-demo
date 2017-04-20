eb8ffc5e446c856093c0e3ce2d6565aafcc36fcb
alcat2008 <ge_yangyang@126.com>
2017-04-20 15:15:32 +0800

    feat: water ripples animation samples

---
+++ animation/src/App.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+import WaterRipples from './water-ripples';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <WaterRipples />
+      </div>
+    );
+  }
+}
+
+export default App;
