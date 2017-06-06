452596b23bf9464e02f8b978755d886fd83f7d77
alcat2008 <ge_yangyang@126.com>
2017-06-06 20:09:18 +0800

    feat: finish dva demo

--- dva/src/models/users.js
+++ dva/src/models/users.js
@@ -1,20 +1,44 @@
-import * as usersService from '../services/users';
+import * as usersService from '../services/usersService';
 
 export default {
   namespace: 'users',
   state: {
     list: [],
     total: null,
+    page: null,
   },
   reducers: {
-    save(state, { payload: { data: list, total } }) {
-      return { ...state, list, total };
+    save(state, { payload: { data: list, total, page } }) {
+      return { ...state, list, total, page };
     },
   },
   effects: {
-    *fetch({ payload: { page } }, { call, put }) {
+    *fetch({ payload: { page = 1 } }, { call, put }) {
       const { data, headers } = yield call(usersService.fetch, { page });
-      yield put({ type: 'save', payload: { data, total: headers['x-total-count'] } });
+      yield put({
+        type: 'save',
+        payload: {
+          data,
+          total: parseInt(headers['x-total-count'], 10),
+          page: parseInt(page, 10),
+        },
+      });
+    },
+    *reload(action, { put, select }) {
+      const page = yield select(state => state.users.page);
+      yield put({ type: 'fetch', payload: { page } });
+    },
+    *remove({ payload: id }, { call, put }) {
+      yield call(usersService.remove, id);
+      yield put({ type: 'reload' });
+    },
+    *patch({ payload: { id, values } }, { call, put }) {
+      yield call(usersService.patch, id, values);
+      yield put({ type: 'reload' });
+    },
+    *create({ payload: values }, { call, put }) {
+      yield call(usersService.create, values);
+      yield put({ type: 'reload' });
     },
   },
   subscriptions: {
