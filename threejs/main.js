067efcaf960f198f514cd8fcb8160d3021295138
alcat2008 <simon.ge@plus.ai>
2019-08-15 15:02:13 +0800

    feat: add paper demo

--- threejs/main.js
+++ threejs/main.js
@@ -9,8 +9,11 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 var geometry = new THREE.CubeGeometry(1,1,1);
 var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-var cube = new THREE.Mesh(geometry, material); scene.add(cube);
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
 camera.position.z = 5;
+
 function render() {
   requestAnimationFrame(render);
   cube.rotation.x += 0.1;
